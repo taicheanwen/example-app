85222a3a94cae193e71fdd6f019b5646882b4b57
taicheanwen <jennytai43@gmail.com>
2023-12-11 15:11:45 +0800

    update2

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
 <?php
-require __DIR__ . '/../public/index.php';
+ 
+// Load the Laravel application
+require _DIR_ . '/../vendor/autoload.php';
+$app = require_once _DIR_ . '/../bootstrap/app.php';
+ 
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request,$response);
